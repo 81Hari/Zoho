3e856362c33ee3c9e74b4b65ca0060e6026e0895
SRI HARI M <85776440+81Hari@users.noreply.github.com>
2022-11-21 14:28:31 +0530

    Update OddEven.java

--- Simple Coding/OddEven/OddEven.java
+++ Simple Coding/OddEven/OddEven.java
@@ -8,38 +8,25 @@ public class OddEven {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			
 			// User Input: size of the array
-			System.out.print("Enter the size for the array: ");
-			int N = Integer.parseInt(br.readLine());
+			System.out.print("Enter the Number: ");
+			Long N = Long.parseLong(br.readLine());
 			
-			int arr[] = new int[N];
-			
-			// User Input: elements of the array
-			System.out.print("\nEnter the elements for the array: ");
-			String rawInput[] = br.readLine().split(" ");
+		
 			
 			br.close();
 			
-			
-			// is size of the array is greater than the number of elements entered. if yes reduce the size of the array else take first 'N' elements. 
-			if(N>rawInput.length) {
-				N = rawInput.length;
-			}
-			
-			
-			for(int i=0;i<N;i++) {	
-				arr[i] = Integer.parseInt(rawInput[i]);
-			}
-			
 			int noOfOdd,noOfEven;
 			noOfOdd = noOfEven = 0;
-			
-			for(int i=0;i<N;i++) {
-				if(arr[i]%2==0) {
+			int temp;
+			while(N!=0) {
+				temp = (int)(N%10);
+				if(temp%2==0) {
 					noOfEven++;
 				}
 				else {
 					noOfOdd++;
 				}
+				N = N/10;
 			}
 			System.out.printf("ODD: %d EVEN: %d",noOfOdd,noOfEven);
 		}
